247e1c91db5968464f3f8bd8d761f519d73670c8
chiragbablani0 <chiragsonubablani@karunya.edu.in>
2020-05-25 11:32:23 +0530

    new code

--- app/Support/Collection.php
+++ app/Support/Collection.php
@@ -5,8 +5,18 @@ namespace App\Support;
 class Collection
 {
 	protected $items = [];
+
+	public function __construct(array $items=[])
+	{
+		$this->items = $items;
+	}
 	public function get()
 	{
 		return $this->items;
+	}
+
+	public function count()
+	{
+		return count($this->items);
 	}
 }
